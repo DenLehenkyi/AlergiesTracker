983afa67c6ae5afb90dca79cbaf620b8adb297f3
YarynaPankiv <pankiv.yaryna00@gmail.com>
2024-05-20 14:43:28 +0300

    navigation

--- AlergiesTracker/screens/HomeScreen.js
+++ AlergiesTracker/screens/HomeScreen.js
@@ -3,15 +3,19 @@ import { View, Text, TouchableOpacity, StyleSheet } from "react-native";
 import AsyncStorage from "@react-native-async-storage/async-storage";
 import { MaterialIcons } from "@expo/vector-icons"; // Потрібно встановити expo/vector-icons
 import MyDrawer from "./MyDrawer";
+import { Button } from "react-native-paper";
+import { useNavigation } from "@react-navigation/native";
 
 export default function HomeScreen() {
   const [userData, setUserData] = useState(null);
-
+  const navigation = useNavigation();
 
 
   return (
-    
+    <>
     <MyDrawer></MyDrawer>
+    <Button   onPress={() => navigation.navigate('AddAllergy')} >Додати алергію</Button>
+    </>
    
   );
 }
